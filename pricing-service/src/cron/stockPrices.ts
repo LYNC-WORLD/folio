b6b937bcf6316ae8c8114517a168490e29ba382a
vaibhav03joshi <vaibhav03joshi@gmail.com>
2026-09-22 22:37:42 +0530

    user and stock api

--- pricing-service/src/cron/stockPrices.ts
+++ pricing-service/src/cron/stockPrices.ts
@@ -3,7 +3,7 @@ import { updateStockPrices } from "../services/stockPrice.service";
 
 export function startStockPriceCron() {
   // Every minute
-  cron.schedule("5 * * * *", async () => {
+  cron.schedule("/5 * * * *", async () => {
     console.log("Running stock price update...");
 
     await updateStockPrices();
