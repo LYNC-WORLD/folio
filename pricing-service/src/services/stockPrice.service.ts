ba07e3b7d29ac4d98d880a494dbf0711cb3d42bc
vaibhav03joshi <vaibhav03joshi@gmail.com>
2026-09-24 15:57:36 +0530

    pricing service added prestocks

--- pricing-service/src/services/stockPrice.service.ts
+++ pricing-service/src/services/stockPrice.service.ts
@@ -9,53 +9,10 @@ interface XStocksPriceResponse {
 
 export async function updateStockPrices(): Promise<void> {
   try {
-    const stocks = await prisma.stocks.findMany({
-      select: {
-        tokenAddress: true,
-        symbol: true,
-        name: true,
-      },
-    });
-
-    console.log(`Updating ${stocks.length} stocks...`);
-
-    for (const stock of stocks) {
-      try {
-        const url = `${XSTOCKS_API}/public/assets/${encodeURIComponent(
-          stock.symbol,
-        )}/price-data`;
-
-        const response = await fetch(url);
-
-        if (!response.ok) {
-          console.error(`Failed to fetch ${stock.symbol}: ${response.status}`);
-          continue;
-        }
-
-        const data = (await response.json()) as XStocksPriceResponse;
-
-        // console.log(`${stock.symbol}:`, data);
-
-        if (data.quote === undefined || data.quote === null) {
-          console.error(`No price returned for ${stock.symbol}`);
-          continue;
-        }
-
-        await prisma.stocks.update({
-          where: {
-            tokenAddress: stock.tokenAddress,
-          },
-          data: {
-            price: String(data.quote),
-          },
-        });
-
-        // console.log(`Updated ${stock.symbol} -> ${data.quote}`);
-      } catch (error) {
-        console.error(`Error updating ${stock.symbol}:`, error);
-      }
-    }
-
+    await Promise.all([
+      updateUSStocksPrice(),
+      updatePreStocksPrice
+    ]);
     console.log("Stock price update completed");
   } catch (error) {
     console.error("Stock price cron failed:", error);
@@ -122,11 +79,121 @@ export async function updateDailyOpenPrices(): Promise<void> {
   }
 }
 
-// (async () => {
-//   try {
-//     const data = await updateStockPrices();
-//     console.log(data);
-//   } catch (error) {
-//     console.error("Error executing async code:", error);
-//   }
-// })();
+async function updateUSStocksPrice() {
+  const usStocks = await prisma.stocks.findMany({
+    where: { stockType: "USStock" },
+    select: {
+      tokenAddress: true,
+      symbol: true,
+      name: true,
+    },
+  });
+
+  console.log(`Updating ${usStocks.length} stocks...`);
+
+  for (const stock of usStocks) {
+    try {
+      const url = `${XSTOCKS_API}/public/assets/${encodeURIComponent(
+        stock.symbol,
+      )}/price-data`;
+
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        console.error(`Failed to fetch ${stock.symbol}: ${response.status}`);
+        continue;
+      }
+
+      const data = (await response.json()) as XStocksPriceResponse;
+
+      // console.log(`${stock.symbol}:`, data);
+
+      if (data.quote === undefined || data.quote === null) {
+        console.error(`No price returned for ${stock.symbol}`);
+        continue;
+      }
+
+      await prisma.stocks.update({
+        where: {
+          tokenAddress: stock.tokenAddress,
+        },
+        data: {
+          price: String(data.quote),
+        },
+      });
+      // console.log(`Updated ${stock.symbol} -> ${data.quote}`);
+    } catch (error) {
+      console.error(`Error updating ${stock.symbol}:`, error);
+    }
+  }
+  return;
+}
+
+async function updatePreStocksPrice() {
+  const usStocks = await prisma.stocks.findMany({
+    where: { stockType: "USStock" },
+    select: {
+      tokenAddress: true,
+      symbol: true,
+      name: true,
+    },
+  });
+
+  console.log(`Updating ${usStocks.length} stocks...`);
+
+  for (const stock of usStocks) {
+    try {
+      const response = await fetch("https://prestocks.com/api/prestocks");
+      if (!response.ok) {
+        throw new Error(
+          `PreStocks API failed: ${response.status} ${response.statusText}`,
+        );
+      }
+      const apiStocks = (await response.json()) as PreStock[];
+      const dbStocks = await prisma.stocks.findMany({
+        where: {
+          stockType: "PreIPO",
+        },
+      });
+      const apiStockMap = new Map(
+        apiStocks.map((stock) => [stock.symbol, stock]),
+      );
+      for (const dbStock of dbStocks) {
+        const apiStock = apiStockMap.get(dbStock.symbol);
+
+        if (!apiStock) {
+          console.warn(`No PreStocks data found for ${dbStock.symbol}`);
+          continue;
+        }
+
+        await prisma.stocks.update({
+          where: {
+            tokenAddress: dbStock.tokenAddress,
+          },
+          data: {
+            price: String(apiStock.tokenPrice),
+          },
+        });
+
+        console.log(`Updated ${dbStock.symbol}: ${apiStock.tokenPrice}`);
+      }
+      console.log("PreIPO stock price update completed");
+      console.log(`Found ${dbStocks.length} preIPO stocks in DB`);
+    } catch (error) {
+      console.error(`Error updating ${stock.symbol}:`, error);
+    }
+  }
+  return;
+}
+
+interface PreStock {
+  name: string;
+  symbol: string;
+  image: string;
+  contract_address: string;
+  markPrice: number;
+  markValuation: number;
+  tokenPrice: number;
+  impliedValuation: number;
+  supply: number;
+}
